b27f365e600f70e498726d06d783e10a08414294
zhangh <zhihy123@qq.com>
2016-03-31 14:08:11 +0800

    add resource config

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports ={
+  //  生产代码目录
+  'build':'./build/**/*',
+  //  需要引入的外部库文件目录
+  'libs':'./libs/**/',
+  //  图片文件夹
+  'img':['./src/images/*','./src/images/**/*'],
+  //  html模版引擎
+  'html':['./src/views/**/*.jade','./src/views/*.jade','!./src/views/layout/*jade'],
+  //  less文件夹
+  'less':'./src/less/*.less',
+  //  客户端使用的js
+  'scripts':['./src/js/*.js','./src/js/**/*.js']
+}
